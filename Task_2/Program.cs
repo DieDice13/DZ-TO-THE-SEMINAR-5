d681edbc001a9e425266ebf842f65092e2af2b0c
Ruslan <die.dice.2003@mail.ru>
2023-09-25 23:57:34 +0600

    The second task is solved

---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.Clear();
+
+int sum = 0; 
+
+int[] GetArray(string message, int min, int max)
+{
+    System.Console.Write(message);
+    int size = int.Parse(Console.ReadLine());
+    int[] array = new int[size];
+    Random random= new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int SumUnPos(int[] array)
+{
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = GetArray("Введите длину массива: ", 1, 99);
+System.Console.WriteLine(string.Join(", ", array));
+SumUnPos(array);
+System.Console.WriteLine($"Сумма элементов стоящих на нечетных позиция равна: {sum}");
